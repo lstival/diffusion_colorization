4ab92339f3299fa00a3b8745138ebc49655e5af5
lstival <leandroteso@gmail.com>
2023-05-04 21:26:24 -0300

    DIffusion with pre trained VAE

--- train_diffusion.py
+++ train_diffusion.py
@@ -7,6 +7,7 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import logging
 from torch import optim
+import VAE as vae
 
 # Load the Network to increase the colorization
 import torch
@@ -15,78 +16,33 @@ import torch.nn as nn
 from ViT import Vit_neck
 # import torchvision.models as models
 from u_net import Encoder, Decoder
+from torchvision.models import vit_b_16
 
-class DiffusionModels():
-    def __init__(self, rever_diffusion=False) -> None:
-        self.rever_diffusion = rever_diffusion
+def valid_model(diffusion, diffusion_model, prompt, dataroot, criterion, epoch, logger, l, i):
+    ### Load dataset 
+    dataLoader = ld.ReadData()
+    dataloader = dataLoader.create_dataLoader(dataroot, image_size, batch_size, shuffle=True)
 
-    def train_vit_neck(self, in_ch, c_out, noise_steps, time_dim, pretained_name):
-        """
-        IF vit neck is true, return the instance of Encoder and Decoder pre trained.
-        Also load the Vitneck trained during the encoder and Decoder train.
+    with torch.no_grad():
+        data = next(iter(dataloader))
+        img, img_gray, key_frame, _ = create_samples(data)
 
-        Valid if rever diffusion is True, else return the classical U_net to reverse Diffusion.
-        The return is a tuple where all models are present in the order:
+        gt_img = img.to(device)
+        val_labels = prompt(key_frame)
+        input_img = img_gray.to(device)
 
-        feature_model, decoder, color_neck, diffusion, diffusion_model
+        latens = vae.pil_to_latents(input_img)
 
-        """
-        feature_model = Encoder(c_in=3, c_out=in_ch//2, return_subresults=True, img_size=image_size).to(device)
-        feature_model = load_trained_weights(feature_model, pretained_name, "feature")
-        feature_model.eval()
+        val_t = diffusion.sample_timesteps(latens.shape[0]).to(device)
+        val_x_t, val_noise = diffusion.noise_images(latens, val_t)
 
-        decoder = Decoder(c_in=in_ch, c_out=3, img_size=image_size, vit_neck=True).to(device)
-        decoder = load_trained_weights(decoder, pretained_name, "decoder")
-        decoder.eval()
+        ### Predict the noise 
+        predicted_noise = diffusion_model(val_x_t, val_t, val_labels).half()
 
-        color_neck = Vit_neck(batch_size=batch_size, image_size=image_size, out_chanels=int((in_ch//2)*8*8))
-        color_neck = load_trained_weights(color_neck, pretained_name, "vit_neck")
-        color_neck.eval()
+        ### Meansure the difference between noise predicted and realnoise
+        val_loss = criterion(predicted_noise, val_noise)
 
-        ### Diffusion process
-        diffusion = Diffusion(img_size=image_size//8, device=device, noise_steps=noise_steps)
-        diffusion_model = UNet_conditional(c_in=in_ch//2, c_out=in_ch//2, time_dim=time_dim, img_size=image_size//8).to(device)
-        diffusion_model.train()
-
-        ### Labels generation
-        prompt = Vit_neck(batch_size=batch_size, image_size=image_size, out_chanels=time_dim).to(device)
-        prompt = load_trained_weights(prompt, vit_name, "vit_constrative", model_path="models_vit")
-        prompt.eval()
-
-        return (feature_model, decoder, color_neck, diffusion, diffusion_model)
-
-    def train_only_diffusion(self, noise_steps, time_dim):
-        """
-        Create the diffusion model responsable to create the noise,
-        and create the reverse network
-        """
-        diffusion = Diffusion(img_size=image_size, device=device, noise_steps=noise_steps)
-        diffusion_model = UNet_conditional(c_in=3, c_out=3, time_dim=time_dim, img_size=image_size).to(device)
-        diffusion_model.train()
-
-        return (diffusion, diffusion_model)
-
-    def train_default(self, pretained_name, noise_steps, time_dim, image_size):
-        """
-        Create the diffusion model to be trained, using the Encoder and Decaoder pre trained.
-        Return a tuple contaning all models
-        """
-        ### Encoder and decoder models
-        feature_model = Encoder(c_in=3, c_out=in_ch//2, return_subresults=True, img_size=image_size).to(device)
-        feature_model = load_trained_weights(feature_model, pretained_name, "feature")
-        feature_model.eval()
-
-        decoder = Decoder(c_in=in_ch, c_out=3, img_size=image_size).to(device)
-        decoder = load_trained_weights(decoder, pretained_name, "decoder")
-        decoder.train()
-
-        ### Diffusion process
-        diffusion = Diffusion(img_size=image_size//8, device=device, noise_steps=noise_steps)
-        diffusion_model = UNet_conditional(c_in=in_ch//2, c_out=in_ch//2, time_dim=time_dim, img_size=image_size//8).to(device)
-        diffusion_model.train()
-
-        return (feature_model, decoder, diffusion, diffusion_model)
-
+    return val_loss, [val_labels]
 
 class TrainDiffusion():
     def __init__(self, dataroot, image_size, time_dim) -> None:
@@ -125,29 +81,36 @@ class TrainDiffusion():
         ## Load Dataset
         dataloader = self.read_datalaoder()
 
-        ## Load Vit prompt
-        ### Labels generation
-        prompt = Vit_neck(batch_size=batch_size, image_size=image_size, out_chanels=time_dim).to(device)
-        prompt = load_trained_weights(prompt, vit_name, "vit_constrative", model_path="models_vit")
+        # Load Vit prompt
+        ## Labels generation
+        # prompt = Vit_neck(batch_size=batch_size, image_size=image_size, out_chanels=time_dim).to(device)
+        # prompt = load_trained_weights(prompt, vit_name, "vit_constrative", model_path="models_vit")
+        # prompt = vit_b_16(weights="ViT_B_16_Weights.IMAGENET1K_V1").to("cuda")
+        prompt = Vit_neck().to("cuda")
         prompt.eval()
 
         ## Load Models
         ### Encoder and decoder models
-        feature_model = Encoder(c_in=3, c_out=in_ch//2, return_subresults=True, img_size=image_size).to(device)
-        feature_model = load_trained_weights(feature_model, pretained_name, "feature")
-        feature_model.eval()
+        # feature_model = Encoder(c_in=3, c_out=out_ch//2, return_subresults=True, img_size=image_size).to(device)
+        # feature_model = load_trained_weights(feature_model, pretained_name, "feature")
+        # feature_model.eval()
 
-        decoder = Decoder(c_in=in_ch, c_out=3, img_size=image_size).to(device)
-        decoder = load_trained_weights(decoder, pretained_name, "decoder")
-        decoder.eval()
+        # decoder = Decoder(c_in=out_ch, c_out=3, img_size=image_size).to(device)
+        # decoder = load_trained_weights(decoder, pretained_name, "decoder")
+        # decoder.eval()
 
         ### Diffusion process
         diffusion = Diffusion(img_size=image_size//8, device=device, noise_steps=noise_steps)
-        diffusion_model = UNet_conditional(c_in=in_ch//2, c_out=in_ch//2, time_dim=time_dim, img_size=image_size//8).to(device)
+        diffusion_model = UNet_conditional(c_in=4, c_out=4, time_dim=time_dim, img_size=image_size//8).to(device)
         diffusion_model.train()
 
         params_list = diffusion_model.parameters()
         optimizer = optim.AdamW(params_list, lr=lr)
+        # optimizer = torch.optim.SGD(params_list, lr=lr, momentum=0.9)
+        # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+        # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = epochs,  eta_min = 1e-4)
+        lmbda = lambda epoch: 0.95
+        scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lmbda)
 
         ## Train Loop
         for epoch in range(epochs):
@@ -167,39 +130,54 @@ class TrainDiffusion():
                 ### Get the video Key frame
                 key_frame = img_color.to(device)
 
-                ### Labels to create sample from noise
+                ## Labels to create sample from noise
                 labels = prompt(key_frame)
 
-                ### Encoder (create feature representation)
-                gt_out, skips = feature_model(input_img)
+                ## Encoder (create feature representation)
+
+                latens = vae.pil_to_latents(input_img)
+
+                # gt_out, skips = feature_model(input_img)
 
                 ### Generate the noise using the ground truth image
-                t = diffusion.sample_timesteps(gt_out.shape[0]).to(device)
-                x_t, noise = diffusion.noise_images(gt_out, t)
+                t = diffusion.sample_timesteps(latens.shape[0]).to(device)
+                x_t, noise = diffusion.noise_images(latens, t)
 
                 ### Predict the noise 
-                predicted_noise = diffusion_model(x_t, t, labels)
+                predicted_noise = diffusion_model(x_t, t, labels).half()
 
                 ### Meansure the difference between noise predicted and realnoise
                 loss = criterion(predicted_noise, noise)
+
+                val_loss, val_data = valid_model(diffusion, diffusion_model, prompt, valid_dataroot, criterion, epoch, logger, l, i)
 
                 # Gradient Steps and EMA model criation
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-                pbar.set_postfix(MSE=loss.item(), epochs=epoch)
-                logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
+                pbar.set_postfix(MSE=loss.item(), Val_Loss = val_loss.item(), epochs=epoch)
+                logger.add_scalar("Loss", loss.item(), global_step=epoch * l + i)
+                logger.add_scalar("Val_Loss", val_loss.item(), global_step=epoch * l + i)
+            
+            scheduler.step()
 
             if epoch % 10 == 0:
-                l = 5
-                if len(labels) < l:
-                    l = len(labels)
+                # l = 5
+                # if (gt_out.shape[0]) < l:
+                l = (gt_img.shape[0])
 
-                x = diffusion.sample(diffusion_model, n=len(labels), labels=labels, in_ch=in_ch//2, create_img=False)
+                # x = diffusion.sample(diffusion_model, labels=labels, n=l, in_ch=out_ch//2, create_img=False)
+                x = diffusion.sample(diffusion_model, labels=labels, n=l, in_ch=4, create_img=False).half()
+                # plot_img = decoder((x, skips))
+                # plot_img = tensor_2_img(plot_img)
+                plot_img = vae.latents_to_pil(x)
 
-                plot_img = decoder((x, skips))
-                
-                plot_img = tensor_2_img(plot_img)
+                ### Plot Validation
+                # x = diffusion.sample(diffusion_model, labels=val_data[0], n=val_data[0].shape[0], in_ch=out_ch//2, create_img=False)
+                x = diffusion.sample(diffusion_model, labels=val_data[0], n=val_data[0].shape[0], in_ch=4, create_img=False).half()
+                val_plot_img = vae.latents_to_pil(x)
+                # val_plot_img = decoder((x, val_data[1]))
+                # val_plot_img = tensor_2_img(val_plot_img)
                 
                 ### Creating the Folders
                 pos_path_save = os.path.join("unet_results", run_name)
@@ -210,36 +188,42 @@ class TrainDiffusion():
 
                 ### Ploting and saving the images
                 plot_images(tensor_2_img(gt_img[:l]))
-                plot_images(plot_img[:l])
-                save_images(plot_img, os.path.join("unet_results", run_name, f"{epoch}.jpg"))
+                plot_images_2(plot_img[:l])
+                plot_images_2(val_plot_img[:l])
+                save_images_2(plot_img, os.path.join("unet_results", run_name, f"{epoch}.jpg"))
                 # Save the models
                 torch.save(diffusion_model.state_dict(), os.path.join("unet_model", run_name, f"ckpt.pt"))
-                torch.save(feature_model.state_dict(), os.path.join("unet_model", run_name, f"feature.pt"))
-                torch.save(decoder.state_dict(), os.path.join("unet_model", run_name, f"decoder.pt"))
-                torch.save(prompt.state_dict(), os.path.join("unet_model", run_name, f"prompt.pt"))
+                # torch.save(feature_model.state_dict(), os.path.join("unet_model", run_name, f"feature.pt"))
+                # torch.save(decoder.state_dict(), os.path.join("unet_model", run_name, f"decoder.pt"))
+                # torch.save(prompt.state_dict(), os.path.join("unet_model", run_name, f"prompt.pt"))
 
         logger.close()
 
 if __name__ == "__main__":
+    seed = 2023
+    torch.manual_seed(seed)
+
     model_name = get_model_time()
-    run_name = f"UNET_k_{model_name}"
-    noise_steps = 1000
-    time_dim=1024
+    run_name = f"UNET_d_{model_name}"
+    noise_steps = 200
+    time_dim=7680
     lr=2e-4
     device="cuda"
-    image_size=128
-    batch_size=32
+    image_size=224
+    batch_size=8
     in_ch=256
+    out_ch=256
     
     # # dataroot = r"C:\video_colorization\data\train\COCO_val2017"
     # # dataroot = r"C:\video_colorization\data\train\mini_kinetics"
     # dataroot = r"C:\video_colorization\data\train\mini_kinetics"
     # # dataroot = r"C:\video_colorization\data\train\rallye_DAVIS"
 
-    vit_name = "VIT_20230426_222111"
-    pretained_name = "UNET_20230426_002220"
-    dataroot = r"C:\video_colorization\data\train\few_kinetics"
-    epochs = 201
+    # vit_name = "VIT_20230429_131814"
+    pretained_name = "UNET_20230502_130014"
+    dataroot = r"C:\video_colorization\data\train\drone_DAVIS"
+    valid_dataroot = r"C:\video_colorization\data\train\rallye_DAVIS"
+    epochs = 501
 
     training = TrainDiffusion(dataroot, image_size, time_dim)
     training.train(epochs, lr)
